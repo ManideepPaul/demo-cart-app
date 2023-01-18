61988016671e489d5b8dd70e708da8cd1af10500
Manideep <17101992m@gmail.com>
2023-01-18 21:44:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+import { Container } from 'react-bootstrap';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Cancel from './pages/Cancel';
+import Store from './pages/Store';
+import Success from './pages/Success';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NavbarComponent></NavbarComponent>
+
+      {/* This is for routing through pages */}
+      <BrowserRouter>
+        <Routes>
+          {/* index means / or "home page" */}
+          <Route index element={<Store />} />
+
+          {/* path mean "/success" will get you <Success /> page */}
+          <Route path='success' element={<Success />} />
+          <Route path='cancel' element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
