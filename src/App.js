795969300b94fcc019626e799169c067ddfd2741
Manideep <17101992m@gmail.com>
2023-01-19 12:53:31 +0530

    Whole app is wrapped under cart provider

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/Navbar';
 import { Container } from 'react-bootstrap';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cancel from './pages/Cancel';
 import Store from './pages/Store';
 import Success from './pages/Success';
+import CartProvider from './CartContext';
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent></NavbarComponent>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
 
-      {/* This is for routing through pages */}
-      <BrowserRouter>
-        <Routes>
-          {/* index means / or "home page" */}
-          <Route index element={<Store />} />
+        {/* This is for routing through pages */}
+        <BrowserRouter>
+          <Routes>
+            {/* index means / or "home page" */}
+            <Route index element={<Store />} />
 
-          {/* path mean "/success" will get you <Success /> page */}
-          <Route path='success' element={<Success />} />
-          <Route path='cancel' element={<Cancel />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+            {/* path mean "/success" will get you <Success /> page */}
+            <Route path='success' element={<Success />} />
+            <Route path='cancel' element={<Cancel />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
