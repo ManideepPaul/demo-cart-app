7b364b47eaeb479834c27be95331555b4379f42d
manideep <17101992m@gmail.com>
2023-01-19 22:48:09 +0530

    created a CartProduct component and user it inside the modalBody in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
-import { useState } from 'react';
-import { Button, Container, Navbar, Modal } from 'react-bootstrap'
+import { useState, useContext } from 'react';
+import { Button, Container, Navbar, Modal } from 'react-bootstrap';
+import { CartContext } from '../CartContext';
+import CartProduct from './CartProduct';
 
 function NavbarComponent() {
+    const cart = useContext(CartContext)
+
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true)
+    const handleShow = () => setShow(true);
+
+    const productCount = cart.items.reduce((sum, product) => sum + product.quantity, 0)
     return (
         <>
             <Navbar>
                 <Navbar.Brand>Shopping Cart</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className='justify-content-end'>
-                    <Button onClick={handleShow}>Cart 0 Items</Button>
+                    <Button onClick={handleShow}>Cart {productCount} Items</Button>
                 </Navbar.Collapse>
             </Navbar>
             <Modal show={show} onHide={handleClose}>
@@ -19,7 +25,20 @@ function NavbarComponent() {
                     <Modal.Title>Shopping Cart</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h1>This is the modal body</h1>
+                    {
+                        productCount > 0 ?
+                            <>
+                            <p>Items in your cart:</p>
+                            {cart.items.map((currentProduct, index) => (
+                                <CartProduct key={index} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+                            ))}
+
+                            <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+                            <Button variant='success'>Purchase items</Button>
+                            </>
+                            :
+                            <h1>Nothing in your cart</h1>
+                    }
                 </Modal.Body>
             </Modal>
         </>
